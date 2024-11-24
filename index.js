33993fb7e35116988ac7b0476c684858dac746ff
Riko <riko.fluchel@gmail.com>
2024-11-24 12:35:41 -0500

    complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(name, event){
+    const messages = []
+    for (let i = 0; i < name.length; i++) {
+        const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!` ;
+        messages.push(message);
+    }
+    return messages 
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(number){
+    let i = 0;
+    while(i <= number){
+        console.log(number - i);
+        i++;
+    }
+}
+
+countDown(10);
